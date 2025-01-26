689aac336aaee45d958978bfefe80cc552504f48
frankzeal33 <frankzeal33@gmail.com>
2025-01-26 19:58:33 +0100

    added

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -23,7 +23,7 @@ const Dashboard = () => {
     <div className='p-10 pt-24 md:px-20 lg:px-32'>
       <h2 className='font-bold text-3xl'>My Resume</h2>
       <p>Start Creating AI resume for your next job role</p>
-      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mt-10 gap-5'>
+      <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mt-10 gap-5'>
         <AddResume/>
         {
           resumeList.length > 0 && resumeList.map((resume, index) => (
